7598897eb8f66847f8558b46a08f9cff7d45fa90
adamkr98 <adamkrynski2@gmail.com>
2023-10-01 11:49:52 +0200

    added some styling

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ let addItem = () => {
 
     let optionContainer = document.createElement('div');
     optionContainer.classList.add('optionContainer');
-    
+
     newItemContainer.appendChild(optionContainer);
     optionContainer.append(changeBtn, removeBtn);
 
@@ -75,11 +75,14 @@ let isItemDone = (newItemContainer, newItem) => {
     newItemContainer.isDone;
 
     if (newItemContainer.isDone) {
-        newItemContainer.style.backgroundColor = "transparent";
+        newItemContainer.style.background = "transparent";
         newItem.style.textDecoration = "none";
+        newItem.style.color = "black";
+
     } else {
-        newItemContainer.style.backgroundColor = "#20FC8F";
+        newItemContainer.style.background = `linear-gradient(135deg, rgba(35,60,242,1) 7%, rgba(234,68,82,1) 35%, rgba(36,99,142,1) 96%)`;
         newItem.style.textDecoration = "line-through";
+        newItem.style.color = "white";
     }
     newItemContainer.isDone = !newItemContainer.isDone;
 };
